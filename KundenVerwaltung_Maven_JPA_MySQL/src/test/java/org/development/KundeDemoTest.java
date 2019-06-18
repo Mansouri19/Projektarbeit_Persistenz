9c1622b9cbe5a3a4787a8769b8854b4013db0c06
Mansouri <afh.mansouri@gmail.com>
2019-06-18 20:32:21 +0200

    update

---
+++ KundenVerwaltung_Maven_JPA_MySQL/src/test/java/org/development/KundeDemoTest.java
@@ -0,0 +1,14 @@
+package org.development;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KundeDemoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
