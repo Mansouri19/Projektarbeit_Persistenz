86c25cdc779a08779a022ba698ad29b59416f8f8
Mansouri <afh.mansouri@gmail.com>
2019-06-25 21:39:51 +0200

    update

--- KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
+++ KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.paint.Color;
@@ -38,7 +39,7 @@ public class MainController {
 	private DatePicker geburtsdatum_tf;
 
 	@FXML
-	private ListView<?> kundenList_listView;
+    private TableView<?> kundenList_TableView;
 
 	@FXML
 	private Label status_label;
