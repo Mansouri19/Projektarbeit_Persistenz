46eb20bf7b908b68e03d836210fa9428c34d5b90
amansouri <amansouri@PC46181116.cc-college.gecs.loc>
2019-06-25 11:20:57 +0200

    update

--- KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
+++ KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
@@ -1,5 +1,56 @@
 package development.project.app_javafx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class MainController {
+	  @FXML
+	    private TextField kundenId_tf;
+
+	    @FXML
+	    private TextField vorname_tf;
+
+	    @FXML
+	    private TextField nachname_tf;
+
+	    @FXML
+	    private TextField adresse_tf;
+
+	    @FXML
+	    private TextField telNr_tf;
+
+	    @FXML
+	    private DatePicker geburtsdatum_tf;
+
+	    @FXML
+	    private ListView<?> kundenList_listView;
+
+	    @FXML
+	    private Label status_label;
+
+	    @FXML
+	    void OnKundeAdd(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void OnKundeDelete(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void OnKundeReset(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void OnKundeUpdate(ActionEvent event) {
+
+	    }
+
 
 }
