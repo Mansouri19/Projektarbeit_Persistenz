0bf112d07106c2bb1abf8ce31b74a5a6bf114688
amansouri <amansouri@PC46181116.cc-college.gecs.loc>
2019-06-25 12:15:12 +0200

    update

--- KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
+++ KundenVerwaltung_Maven_JPA_MySQL/src/main/java/development/project/app_javafx/MainController.java
@@ -1,5 +1,8 @@
 package development.project.app_javafx;
 
+import java.time.format.DateTimeFormatter;
+
+import development.project.dao.model.Kunde;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
@@ -8,49 +11,59 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class MainController {
-	  @FXML
-	    private TextField kundenId_tf;
+	@FXML
+	private TextField kundenId_tf;
 
-	    @FXML
-	    private TextField vorname_tf;
+	@FXML
+	private TextField vorname_tf;
 
-	    @FXML
-	    private TextField nachname_tf;
+	@FXML
+	private TextField nachname_tf;
 
-	    @FXML
-	    private TextField adresse_tf;
+	@FXML
+	private TextField adresse_tf;
 
-	    @FXML
-	    private TextField telNr_tf;
+	@FXML
+	private TextField telNr_tf;
 
-	    @FXML
-	    private DatePicker geburtsdatum_tf;
+	@FXML
+	private DatePicker geburtsdatum_tf;
 
-	    @FXML
-	    private ListView<?> kundenList_listView;
+	@FXML
+	private ListView<?> kundenList_listView;
 
-	    @FXML
-	    private Label status_label;
+	@FXML
+	private Label status_label;
 
-	    @FXML
-	    void OnKundeAdd(ActionEvent event) {
+	@FXML
+	void OnKundeAdd(ActionEvent event) {
 
-	    }
+	}
 
-	    @FXML
-	    void OnKundeDelete(ActionEvent event) {
+	@FXML
+	void OnKundeDelete(ActionEvent event) {
 
-	    }
+	}
 
-	    @FXML
-	    void OnKundeReset(ActionEvent event) {
+	@FXML
+	void OnKundeReset(ActionEvent event) {
+		Kunde kunde = new Kunde(null, "", "", "", "", "");
+		updateGuiFrom(kunde);
+	}
 
-	    }
+	@FXML
+	void OnKundeUpdate(ActionEvent event) {
 
-	    @FXML
-	    void OnKundeUpdate(ActionEvent event) {
+	}
 
-	    }
-
+	private void updateGuiFrom(Kunde kunde) {
+		kundenId_tf.setText(String.valueOf(kunde.getKundeId()));
+		vorname_tf.setText(kunde.getVorname());
+		nachname_tf.setText(kunde.getNachname());
+//		geburtsdatum_tf.
+		adresse_tf.setText(kunde.getAdresse());
+		telNr_tf.setText(kunde.getTelNr());
+		
+	}
 
 }
